a92e01b99377eec1f04de4e7d8926ea7c1b3aac7
peashooter45 <balvinpatrick@yahoo.com>
2023-05-02 10:39:39 -0400

    data is mapped and form is added

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,24 +1,8 @@
+import { useState } from "react";
 import { todo } from "../../../models/todo.models";
 
-function Todo(datas: todo[]) {
-  return (
-    <>
-      <div>
-        <h1>Todo</h1>
-        <input type="text" />
-      </div>
-      <div>
-        <ul>
-          {datas.map((data: any) => (
-            <li>{data.title}</li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
-}
 export async function getStaticProps() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const res = await fetch(" http://localhost:3500/todos");
   const datas = await res.json();
 
   return {
@@ -26,6 +10,31 @@ export async function getStaticProps() {
       datas,
     },
   };
+}
+
+function Todo({ datas }: { datas: todo[] }) {
+  const [inputText, setinputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = (e: Event) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Todo</h1>
+        <input onChange={e => setinputText(e.target.value)} type="text" />
+        <button>Add</button>
+      </div>
+      <div>
+        <ul>
+          {datas.map((data: any) => (
+            <li key={data.id}>{data.todo}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Todo;
